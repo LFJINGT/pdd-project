e6837bb6396e3908e3db95e75d11f396d3c8834d
LFJINGT <2360870828@qq.com>
2021-11-08 14:56:20 +0800

    Default Changelist

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+<home-header></home-header>
+</template>
+
+<script>
+import HomeHeader from './components/HomeHeader'
+export default {
+  name: 'Home',
+  components: {HomeHeader}
+}
+</script>
+
+<style scoped>
+
+</style>
